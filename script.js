628f6e5331dfae8f444bb19ac377de116782fe11
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-04-01 00:21:43 -0300

    Final???

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let nPrato, pPrato, nBebida, pBebida, nSobremesa, pSobremesa;
-let precoTotal = 0;
+let pTotal = 0;
 
-function ativarBotao(){
+function ativarBotao() {
     if (document.querySelector(".pratos .selecionado") !== null
         && document.querySelector(".bebidas .selecionado") !== null
         && document.querySelector(".sobremesas .selecionado") !== null) {
-            const botao = document.querySelector(".finalizar button");
-            botao.removeAttribute("disabled");
-            botao.classList.add("ativado");
-            botao.innerHTML = "Fechar pedido";
+        const botao = document.querySelector(".finalizar button");
+        botao.removeAttribute("disabled");
+        botao.classList.add("ativado");
+        botao.innerHTML = "Fechar pedido";
     }
 }
 
@@ -45,21 +45,55 @@ function seletorSobremesa(clicado) {
     ativarBotao();
 }
 
+function confirmarPedido() {
+const sobreposicao = document.querySelector(".sobrepor")
+sobreposicao.classList.remove("escondido");
+
+    const fechaPedido = document.querySelector(".confirmar-pedido");
+    
+    const dados = document.querySelector(".confirmar-pedido")
+    console.log(dados)
+
+    fechaPedido.querySelector(".pratoPedido").innerHTML = nPrato;
+    fechaPedido.querySelector(".bebidaPedido").innerHTML = nBebida;
+    fechaPedido.querySelector(".sobremesaPedido").innerHTML = nSobremesa;
+
+    fechaPedido.querySelector(".pPratoPedido").innerHTML = pPrato;
+    fechaPedido.querySelector(".pBebidaPedido").innerHTML = pBebida;
+    fechaPedido.querySelector(".pSobremesaPedido").innerHTML = pSobremesa;
+
+    pPratoOp = Number(pPrato.replace('R$ ', '').replace(',', '.'));
+    pBebidaOp = Number(pBebida.replace('R$ ', '').replace(',', '.'));
+    pSobremesaOp = Number(pSobremesa.replace('R$ ', '').replace(',', '.'));
+
+    pTotal = (pPratoOp + pBebidaOp + pSobremesaOp).toFixed(2);
+    pTotal = String(pTotal).replace('.', ',');
+    fechaPedido.querySelector(".totalPedido").innerHTML = "R$ " + pTotal;
+
+
+}
+
 function fecharPedido() {
     const name = prompt("Digite o seu nome");
     const endereco = prompt("Digite o seu endereço");
+
+
 
     const mensagem = `Olá, gostaria de fazer o pedido:
     - Prato:  ` + nPrato + `
     - Bebida:  ` + nBebida + `
     - Sobremesa:  ` + nSobremesa + `
-    Total: R$ ` + precoTotal + `
+    Total: R$ ` + pTotal + `
     
     Nome: ` + name + `
     Endereço: ` + endereco;
 
-const mensagemWapp = encodeURIComponent(mensagem);
-console.log(mensagemWapp);
-//mudar depois
-window.open(`https://wa.me/+5562993190165?text=${mensagemWapp}`);
+    const mensagemWapp = encodeURIComponent(mensagem);
+    console.log(mensagemWapp);
+    window.open(`https://wa.me/+5562993190165?text=${mensagemWapp}`);
+}
+
+function cancelarPedido() {
+    const cancelPedido = document.querySelector(".sobrepor");
+    cancelPedido.classList.add("escondido");
 }
